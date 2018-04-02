c6e9b608e52b78cd41eb219f9e28e721ac4472c7
Upepo Mwindaji <upepo.mwindaji@gmail.com>
2018-04-02 23:34:20 +0200

    prepare for deploy to github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,14 @@
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/<repository-name>/'
+  }
+} : {}
+
+
 module.exports = {
+
+  ...routerBase,
 
   modules: [
    ['~/modules/google-tag-manager', { id: 'GTM-TWMCBFD', allowDev: true}],
