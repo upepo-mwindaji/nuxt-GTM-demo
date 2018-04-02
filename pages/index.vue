46752e7f541b306d626622ffd3234723c3adcf04
Upepo Mwindaji <upepo.mwindaji@gmail.com>
2018-04-02 21:50:09 +0200

    added preview link

--- pages/index.vue
+++ pages/index.vue
@@ -16,26 +16,32 @@
         <h2 class="subtitle">
           Demo using GTM module with Nuxt
         </h2>
-        <a to="#"
-        class="button--blue">Activate Debug</a>
+        <a :href="previewUrl" target="_blank" class="button--blue" >Activate Debug</a>
+
       </div>
     </div>
   </section>
 </template>
 
 <script>
+const previewUrlBase = 'https://www.googletagmanager.com/start_preview/gtm?uiv2&id=GTM-TWMCBFD&gtm_auth=a-0uanYFkML7e3v7Vmxpwg&gtm_preview=env-8&gtm_debug=x&url='
 
 export default {
   data() {
     return {
-      title: 'Homepage'
+      title: 'Homepage',
+      previewUrl: ''
     }
   },
   head() {
     return {
       title: `Nuxt GTM Demo | ${this.title}`
     }
+  },
+  mounted () {
+    this.previewUrl = (previewUrlBase + window.location.href )
   }
+
 }
 </script>
 
